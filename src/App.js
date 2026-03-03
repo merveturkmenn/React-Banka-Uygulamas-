be72f14a9c45571f6c415a66fc027f5cd66406dc
Merve Türkmen <merveturkmen4848@gmail.com>
2026-03-03 17:02:54 +0300

    Confetti özelliği eklendi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer, useEffect } from 'react';
 import './App.css'; 
+import confetti from 'canvas-confetti';
 
 
 const reducer = (state, action) => {
@@ -25,7 +26,20 @@ function App() {
     localStorage.setItem('kayitli_bakiye', state.bakiye);
     console.log("Bakiye kasaya kilitlendi:", state.bakiye);
   }, [state.bakiye]); 
- 
+
+const handleParaYatir = () => {
+    
+   
+      confetti({
+        particleCount: 500,
+        spread: 150,
+        origin: { y: 0.6 },
+        colors: ['#e62d99', '#cc2ebc', '#b6d536','#53d8df']
+      });
+    
+    dispatch({ type: 'para_yatir' });
+  };
+
   return (
     <div className="container"> 
       <h1>{state.kullanici}'nin Banka Hesabı</h1>
@@ -38,8 +52,11 @@ function App() {
 
       <div className="button-group">
      
-        <button onClick={() => dispatch({ type: 'para_yatir' })}>
+        <button 
+        
+        onClick={handleParaYatir} >
           100 TL Yatır
+          
         </button>
 
         <button className="withdraw" onClick={() => dispatch({ type: 'para_cek' })}>
