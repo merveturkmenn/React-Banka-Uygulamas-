2b726cb02bbca8236e21aaf3b6a516a04a34dfa2
Merve Türkmen <merveturkmen4848@gmail.com>
2026-03-03 16:00:12 +0300

    useReducer ile banka uygulaması oluşturuldu

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useReducer, useEffect } from 'react';
+import './App.css'; 
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'para_yatir':
+      return { ...state, bakiye: state.bakiye + 100 };
+    case 'para_cek':
+      return { ...state, bakiye: state.bakiye - 100 };
+    case 'sifirla':
+      return { bakiye: 0 };
+    default:
+      return state;
+  }
+};
+const initialBalance = Number(localStorage.getItem('kayitli_bakiye')) || 0;
+
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, { bakiye: initialBalance , kullanici: 'Merve'});
+
+  
+  useEffect(() => {
+    localStorage.setItem('kayitli_bakiye', state.bakiye);
+    console.log("Bakiye kasaya kilitlendi:", state.bakiye);
+  }, [state.bakiye]); 
+ 
+  return (
+    <div className="container"> 
+      <h1>{state.kullanici}'nin Banka Hesabı</h1>
+
+    <h1 className={state.bakiye < 0 ? 'balance danger' : 'balance'}>
+    {state.bakiye} TL
+    </h1>
+
+    {state.bakiye < 0 && <p className="error-msg">⚠️ Eksiye düştünüz!</p>}
+
+      <div className="button-group">
+     
+        <button onClick={() => dispatch({ type: 'para_yatir' })}>
+          100 TL Yatır
+        </button>
+
+        <button className="withdraw" onClick={() => dispatch({ type: 'para_cek' })}>
+          100 TL Çek
+        </button>
+
+        <button className="reset" onClick={() => dispatch({ type: 'sifirla' })}>
+          Sıfırla
+        </button>
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
